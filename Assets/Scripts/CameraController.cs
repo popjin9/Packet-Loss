676fabdffcdc8844c775fcc28efd8b339792268a
popjin9 <33330439+popjin9@users.noreply.github.com>
2018-04-01 11:14:20 +1000

    Basic Camera Movement
    Added Basic Camera follow and some lighting

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Vector3 cameraOffset;
+	public GameObject player;
+
+	void Start () {
+		cameraOffset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + cameraOffset;
+		
+	}
+}
