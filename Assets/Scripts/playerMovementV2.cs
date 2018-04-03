c609a6928190e5d48e0b968d31d8917ead8ee42b
popjin9 <33330439+popjin9@users.noreply.github.com>
2018-04-03 21:27:46 +1000

    Updated Movement

--- Assets/Scripts/playerMovementV2.cs
+++ Assets/Scripts/playerMovementV2.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class playerMovementV2 : MonoBehaviour {
 
-	// Use this for initialization
+	public float moveSpeed;
+	private Vector3 movementInput;
+	private Rigidbody rb;
+
+	private Camera mainCam;
+	private float mainCamRotY;
+	private Vector3 mainCamRot;
+
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
+
+		mainCam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		movementInput = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		rb.AddRelativeForce (movementInput * moveSpeed, ForceMode.Acceleration);
+
+		mainCamRotY = mainCam.transform.eulerAngles.y;
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, mainCamRotY, transform.eulerAngles.z);
+		//print (mainCamRotY);
 	}
 }
